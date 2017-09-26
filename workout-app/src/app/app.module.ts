fba6f453768c0028020e5ba0fa55b8efafe4614f
Mikkel Poulsen <mikkelpoulsen@Mikkels-MacBook-Air.local>
2017-09-26 13:50:41 +0200

    routing skelet

--- workout-app/src/app/app.module.ts
+++ workout-app/src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AppRoutingModule
   ],
   imports: [
     BrowserModule,
